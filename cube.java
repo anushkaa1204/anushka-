c056bf7735037f70eb7e4ebcd281a09e8f86524b
anushkaa1204 <95102373+anushkaa1204@users.noreply.github.com>
2023-02-13 10:16:20 +0530

    Create cube.java

---
+++ cube.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public class CubeofNumber {
+private static Scanner sc;
+public static void main(String[] args) 
+{
+System.out.print(" Please Enter any Number : ");
+number = sc.nextInt();
+number = sc.nextInt();number * number * number;
+System.out.println("\n The Cube of a Given Number  " + number + "  =  " + cube);
+}
